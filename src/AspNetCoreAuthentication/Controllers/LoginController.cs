b044d04f9cee9508708b1c42cd2c1fa19414eb16
pratik8490 <p.kankhara@gmail.com>
2017-03-09 01:25:30 +0530

    project checked in
    aspent core project

---
+++ src/AspNetCoreAuthentication/Controllers/LoginController.cs
@@ -0,0 +1,56 @@
+﻿using AspNetCoreAuthentication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Threading.Tasks;
+
+namespace AspNetCoreAuthentication.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login(string returnUrl)
+        {
+            var model = new LoginViewModel { ReturnUrl = returnUrl };
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var client = new HttpClient())
+                {
+                    LoginModel loginModel = new LoginModel();
+                    loginModel.username = model.Username;
+                    loginModel.password = model.Password;
+
+                    client.BaseAddress = new Uri("http://localhost:64295");
+
+                    HttpResponseMessage response = new HttpResponseMessage();
+                    response = client.PostAsync("/api/account", loginModel, new JsonMediaTypeFormatter()).Result;
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var token = response.Content.ReadAsAsync<UserToken>().Result;
+                        LoginContext.UserToken = token;
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+
+            }
+            ModelState.AddModelError("", "Invalid login attempt");
+            return View(model);
+        }
+    }
+
+    public class LoginModel
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
---
+++ src/AspNetCoreAuthentication/Models/LoginViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreAuthentication.Models
+{
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+
+    public class LoginContext
+    {
+        public static UserToken UserToken { get; set; }
+    }
+    public class UserToken
+    {
+        public bool authenticated { get; set; }
+        public int entityId { get; set; }
+        public string token { get; set; }
+        public string tokenExpires { get; set; }
+    }
+}
